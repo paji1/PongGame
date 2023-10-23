cf2e0744ddaa3039811287cba0bcfd32a47b17a0
paji1 <tahamaroc1@gmail.com>
2023-10-23 18:04:59 +0100

    fix: solve conflict

--- scripts/rebase_All_to_main.sh
+++ scripts/rebase_All_to_main.sh
@@ -8,6 +8,9 @@ for branch in "${branches[@]}"
 do
   git checkout "$branch"
   git rebase main
-  
+  if [ $? -ne 0 ]; then
+    echo "Conflicts occurred on branch $branch. Please resolve them and continue the rebase."
+    exit 3
+  fi
 done
 git checkout main
