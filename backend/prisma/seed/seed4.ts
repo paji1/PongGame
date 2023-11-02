52fbcf5c8a17c17305325cb735eb2af642033beb
paji1 <tahamaroc1@gmail.com>
2023-11-03 00:48:13 +0100

    fix(seeders error)

---
+++ backend/prisma/seed/seed4.ts
@@ -0,0 +1,44 @@
+var fs = require('fs'); 
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+var messages = []
+
+async function seedmessages(messages) {
+  for (let i = 0; i < messages.length ; i++)
+  {
+    await console.log(messages[i])
+    await prisma.$executeRaw`insert into public.messages(room_id, user_id, message) values(${messages[i].room_id}::integer, ${messages[i].user_id}::integer, ${messages[i].message})`
+  }
+  
+  const res = await prisma.$executeRaw`select * from public.messages`
+
+  console.log(res);
+}
+
+const getmessages = () =>
+{
+  fs.createReadStream('/code/prisma/seed/messages.csv', 'utf-8')
+  .on('data',function(csvrow) {
+    csvrow.split('\n').forEach(element => {
+      class obj  {
+        constructor (room_id , user_id, message){
+          this.room_id = room_id;
+          this.user_id = user_id;
+          this.message = message;
+        }
+        room_id ;
+        user_id;
+        message;
+      }
+      const [room_id, user_id, message] = element.split(',')
+      messages.push(new obj(room_id, user_id, message))
+    });
+  }).on('end', () => seedmessages(messages))
+  
+}
+( async() => {
+    await prisma.$connect();
+    await getmessages();
+    await prisma.$disconnect();
+})()
