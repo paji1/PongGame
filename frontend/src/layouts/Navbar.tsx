9c8346bd52c56ead97487659aac84af39da0e110
paji1 <tahamaroc1@gmail.com>
2023-11-03 05:48:37 +0100

    chore: react file structor

---
+++ frontend/src/layouts/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Navbar(){
+    return (<div>
+        <p>Navbar</p>
+    </div>)
+}
+export default Navbar;
+
+
+
---
+++ frontend/src/layouts/Pagecontainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Pagecontainer(){
+    return (<div>
+    <p>Pagecontainer</p>
+    </div>)
+}
+
+export default  Pagecontainer;
