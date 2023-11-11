4924dfb077846a79b78a0c46121c2d6de60655cf
paji1 <tahamaroc1@gmail.com>
2023-11-11 17:17:32 +0100

    add :fecth conversation

--- backend/src/chat/messages/messages.service.ts
+++ backend/src/chat/messages/messages.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { count } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -6,23 +6,39 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class MessagesService {
     constructor(private readonly prisma: PrismaService)
     {}
-    async sendMessage(pid, rid, message)
+    async sendMessage(pid, rid, message: string)
     {
-        
-            const count = await this.prisma.rooms_members.findFirst({
-                where: {AND :[{ roomid: Number(rid) }, {userid : Number(pid)}]}
-            }    
-            )
-            if (count == null ||  count.isblocked || count.ismuted)
-                throw new Error('error sending message');
-            await this.prisma.messages.create({
-                data:{
-                    room_id: Number(rid),
-                    sender_id: Number(pid),
-                    messages: message
-                }
-            })
-            
-
+        const membership = await this.prisma.rooms_members.findFirst({
+            where: {AND :[{ roomid: Number(rid) }, {userid : Number(pid)}]}
+        }    
+        )
+        if (!membership||  membership.isblocked || membership.ismuted)
+        throw new HttpException("cant send message", 403);
+        if (!message.length)
+            throw new HttpException("Not Acceptable", 406);  
+        const msg  = await this.prisma.messages.create({
+            data:{
+                room_id: Number(rid),
+                sender_id: Number(pid),
+                messages: message
+            }
+        })
+        return msg;
+    }
+    async getMessages(pid, rid)
+    {
+        const membership = await this.prisma.rooms_members.findFirst({
+            where: {AND :[{ roomid: Number(rid) }, {userid : Number(pid)}]}
+        }    
+        )
+        if (!membership|| membership.ismuted)
+            throw new HttpException("Unauthorized", 401);
+        const conversation = await this.prisma.messages.findMany({
+            where: {
+                room_id: rid,
+            }
+        })
+        console.log(conversation)
+        return conversation;
     }
 }
