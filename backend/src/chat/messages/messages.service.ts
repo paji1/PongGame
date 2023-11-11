c9fd604d054ec65660a1b690f007f6703c8a669f
paji1 <tahamaroc1@gmail.com>
2023-11-11 16:48:34 +0100

    fix(remove the need of a transaction)

--- backend/src/chat/messages/messages.service.ts
+++ backend/src/chat/messages/messages.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { count } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MessagesService {
     constructor(private readonly prisma: PrismaService)
     {}
-    async sendMessage(pid, rid)
+    async sendMessage(pid, rid, message)
     {
-        const message = ""
-        const res = await this.prisma.$transaction(async (trx) =>{
-            const count = await trx.rooms_members.count({
-                where: { AND :[{ roomid: Number(rid) }, {userid : Number(pid)}] }
-            }
+        
+            const count = await this.prisma.rooms_members.findFirst({
+                where: {AND :[{ roomid: Number(rid) }, {userid : Number(pid)}]}
+            }    
             )
-            if (count != 1)
-                throw new Error('inconsistent data')
-            await trx.messages.create({
+            if (count == null ||  count.isblocked || count.ismuted)
+                throw new Error('error sending message');
+            await this.prisma.messages.create({
                 data:{
                     room_id: Number(rid),
                     sender_id: Number(pid),
@@ -23,6 +23,6 @@ export class MessagesService {
                 }
             })
             
-        })
+
     }
 }
