a69a112e46a53fc7da7a4dfc7a3005876df1de98
paji1 <tahamaroc1@gmail.com>
2023-11-14 16:18:02 +0100

    fix(add prisma module to imports)

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -4,12 +4,14 @@ import { ChatController } from './chat.controller';
 import { RoomsService } from './rooms/rooms.service';
 import { RoomsModule } from './rooms/rooms.module';
 import { MessagesModule } from './messages/messages.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 
 @Module({
   exports: [ChatService],
   providers: [ChatService, RoomsService],
   controllers: [ChatController],
-  imports: [RoomsModule, MessagesModule]
+  
+  imports: [RoomsModule, MessagesModule, PrismaModule]
 })
 export class ChatModule {}
