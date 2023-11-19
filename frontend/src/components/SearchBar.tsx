9b7e11760071586ed00355c476acd3b5bba1885a
paji1 <tahamaroc1@gmail.com>
2023-11-19 23:43:06 +0100

    import svg

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import zoomicon from '../assets/zoomicon.svg'
 
 const SearchBar = () => (
 	<form action="POST">
@@ -14,9 +15,7 @@ const SearchBar = () => (
 				border-solid border-textColor h-[39px] focus:outline-none
 				flex items-center justify-center shadow-buttonShadow
 				`}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-					<path d="M19 6V5H18V4H17V3H7V4H6V5H5V6H4V16H5V17H6V18H7V19H15V21H16V22H19V20H18V19H17V18H18V17H19V16H20V6H19ZM18 14H17V15H16V16H15V17H9V16H8V15H7V14H6V8H7V7H8V6H9V5H15V6H16V7H17V8H18V14Z" fill="#000301"/>
-				</svg>
+				<img src={zoomicon} alt="zoom-icon" />
 			</button>
 		</div>
 	</form>
