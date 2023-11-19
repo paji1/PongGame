a25fd3874ae1bde74a93241488f0b97095c930c7
paji1 <tahamaroc1@gmail.com>
2023-11-19 01:39:53 +0100

    <fix>: Component import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 const App = () => (
-	<Header />
+	<Navbar />
 )
 
 export default App;
