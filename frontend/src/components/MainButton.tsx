c42336288e7dc7a7ce250cf6800af0ab0516ebdf
paji1 <tahamaroc1@gmail.com>
2023-11-19 21:03:21 +0100

    <edit>: border to mainbutoon

--- frontend/src/components/MainButton.tsx
+++ frontend/src/components/MainButton.tsx
@@ -8,7 +8,7 @@ interface buttonVariables {
 const MainButton : React.FC<buttonVariables> = (props) => (
 	<a href={`${props.url}`}
 		className={`bg-buttonColor text-textColor w-full py-2 px-8
-			rounded-full shadow-buttonShadow
+			rounded-full shadow-buttonShadow border-solid border-textColor border-2
 		`
 		}>
 		{props.name}
