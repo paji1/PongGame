225852810e085c9430071431d15c321d6cbd5639
paji1 <tahamaroc1@gmail.com>
2023-11-24 05:23:55 +0100

    generate nest resources for game

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppGateway } from "./app.gateway";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ChatModule } from "./chat/chat.module";
+import { GameModule } from './game/game.module';
 
 @Module({
-	imports: [PrismaModule, ChatModule],
+	imports: [PrismaModule, ChatModule, GameModule],
 	providers: [AppGateway],
 })
 export class AppModule {}
