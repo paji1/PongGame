ecfb149f9e080e72f73e2e539900536f7149e821
paji1 <tahamaroc1@gmail.com>
2023-11-30 00:09:13 +0100

    <add> Notification bar

--- frontend/src/components/sidebar/ToggleSidBar.tsx
+++ frontend/src/components/sidebar/ToggleSidBar.tsx
@@ -16,10 +16,3 @@ export const ToggleSidBar = ({isOpen, setIsOpen}:{isOpen: Boolean, setIsOpen: an
 	</button>
 )
 
-export const HoverDiv = ({toggleChatBar}: {toggleChatBar: any}) => (
-	<div
-	className="fixed inset-0 bg-textColor bg-opacity-50 transition-all duration-300"
-	onClick={() => toggleChatBar()}
-	aria-hidden="true"
-	></div>
-)
