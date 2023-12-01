d2ff683a4f310da37d76b155cfedbb2cbe6677e0
paji1 <tahamaroc1@gmail.com>
2023-12-01 02:36:21 +0100

    <add> notification bar is done!

--- frontend/src/components/notifbar/NotificationBar.tsx
+++ frontend/src/components/notifbar/NotificationBar.tsx
@@ -1,21 +1,37 @@
 import { useState } from "react"
-import { ToggleButton } from "../settings/ToggleSettings"
+import { useContext } from "react"
+import { ToggleButton } from "./ToggleButton"
 import { HoverDiv } from "../Common"
+import { CurrentUser, currentUser } from "../Context/AuthContext"
+import profileplaceholder from "../../assets/profileplaceholder.png"
+import NotificationItem from "./NotificationItem"
+import { INotificaion, InviteType } from "../../types/NotificationItem"
+
 
 
 const NotificationBar = () => {
-	const [isOpen, seIsOpen] = useState(false)
-	// const [peopleOrRooms, setPeopleOrRooms] = useState(false)
-	const [searchSelection, setSearchSelection] = useState(1)
-	const [searchText, setSearchText] = useState("")
+	const [isOpen, seIsOpen] = useState(true)
+	const [ state, setState ] = useState(1)
+
+	const notif: INotificaion = {
+		initiator : {
+			id: 1,
+            nickname: "John",
+			user42: "MGS",
+            avatar: profileplaceholder
+		},
+		inviteDate: new Date(),
+		inviteType: InviteType.GAME,
+		status: 1
+	}
 
 	const toggleChatBar = () => seIsOpen(!isOpen)
 
+	const user: CurrentUser | null = useContext(currentUser)
+
 	return (
 		<>
-		{isOpen && (
-			<HoverDiv toggleChatBar={toggleChatBar} />
-		)}
+		{isOpen && (<HoverDiv toggleChatBar={toggleChatBar} />)}
 
 
 		<ToggleButton isOpen={isOpen} setIsOpen={toggleChatBar} />
@@ -26,23 +42,51 @@ const NotificationBar = () => {
 			font-pixelify
 			${isOpen ? 'transform translate-x-0' : 'transform translate-x-full'}`}
 		>
-			<div className={`flex flex-col gap-2 h-full max-h-full min-h-full `}>
+			<div className={`flex flex-col gap-2 h-full max-h-full min-h-full p-1 divide-x`}>
 
-				<div className={`flex flex-row row h-[50px] min-h-[50px]`}>
-					<div className={`w-1/2 ${searchSelection === 1 ? "border-t-2 border-r-2" : "border-b-2"} border-solid border-textColor
-					cursor-pointer`}
-					onClick={() => {setSearchSelection(1)}}>
-						<h3 className="flex items-center justify-center h-full">Your friends</h3>
+				<div className={`grid grid-cols-10 gap-1 p-1 h-[92px] min-h-[92px] content-evenly
+					cursor-pointer`}>
+					<div className={`col-span-2`} >
+						<img className={`border-2 border-solid border-textColor w-[72px] min-w-[72px] min-h-[72px] h-[72px]`} src={`${!user ? profileplaceholder : user.avatar}`} alt="" />
 					</div>
-					<div className={`w-1/2 ${searchSelection === 2 ? "border-t-2 border-l-2" : "border-b-2"} border-solid border-textColor
-					cursor-pointer`}
-					onClick={() => {setSearchSelection(2)}} >
-						<h3 className="flex items-center justify-center h-full">Chat rooms</h3>
+					<div className={`col-span-4 gap-2 p-2`}>
+						<h2 className={`text-primary font-pixelify font-bold text-lg`}>
+							{user ? user.nickname : "Loading..."}
+						</h2>
+						<p className={`${state === 1 ? "text-sucessColor" : state === 2 ? "text-errorColor" : "text-primary"}
+							text-xs
+						`}>
+							<span>•</span>
+							{state === 1 ? " Online" : state === 2 ? "Offline" : "In-game"}
+						</p>
+					</div>
+
+					<div className="col-span-2 flex items-center justify-center h-full">
+						<button>level</button>
+					</div>
+					<div className="col-span-2 flex items-center justify-center h-full">
+						<button>view profile</button>
 					</div>
 				</div>
+				<hr className="my-1 h-0.5 border-t-0 bg-textColor opacity-100" />
 
-				<div className={`flex-1 min-h-[100px] border-solid border-blue-500 border-2`}>
+				<div className={`flex flex-col flex-1 py-3 min-h-[100px] gap-2 px-2 sm:px-3 overflow-y-scroll`}>
 
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
+					<NotificationItem notif={notif} />
 					
 				</div>
 
