ec052b74ebe750fb936ac4ead2dda039fc28c8fb
paji1 <tahamaroc1@gmail.com>
2023-12-07 00:56:03 +0100

    <fix>: Prisma reconnection error

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [UsersService, PrismaService]
+	imports: [PrismaModule],
+	providers: [UsersService]
 })
 export class UsersModule {}
