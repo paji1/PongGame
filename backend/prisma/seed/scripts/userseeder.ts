54e2e7303b9cf42932f05bcba5e3c4f930682473
paji1 <tahamaroc1@gmail.com>
2023-12-08 02:19:34 +0100

    fix(prisma)

--- backend/prisma/seed/scripts/userseeder.ts
+++ backend/prisma/seed/scripts/userseeder.ts
@@ -7,26 +7,36 @@ import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 var users = []
 
-const getdata = ()=>
-{
-  fs.createReadStream('/code/prisma/seed/csv/users.csv', 'utf-8')
-      .on('data',function(csvrow) {
-         csvrow.split('\n').forEach(element => {
-          users.push(element)
-        });
-      })
-      .on('end', () => seedUsers(users))
-
-}
 
 
 async function seedUsers(users){
+
   for (let i = 0; i < users.length ; i++)
   {
-    await prisma.$executeRaw`insert into public.user(user42) values(${users[i]})`
+    await prisma.$executeRaw`insert into public.user(user42, nickname) values(${users[i].user42},${users[i].nickname} )`
   }
   const res = await prisma.$executeRaw`select * from public.user`
   console.log(res);
+}
+
+const getdata = () =>
+{
+  fs.createReadStream('./prisma/seed/csv/users.csv', 'utf-8')
+  .on('data',function(csvrow) {
+    csvrow.split('\n').forEach(element => {
+      class obj  {
+        constructor (user42 , nickname){
+          this.user42 = user42;
+          this.nickname = nickname;
+        }
+        user42 ;
+        nickname ;
+      }
+      const [user42,nickname] = element.split(',')
+      users.push(new obj(user42,nickname))
+    });
+  }).on('end', () => seedUsers(users))
+  
 }
 
 ( async() => {
