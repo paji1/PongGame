7d5d9bc9cdd366f2a0b746955fb92226b5bb5a39
paji1 <tahamaroc1@gmail.com>
2023-12-11 01:49:37 +0100

    still working on the game

--- frontend/src/components/game/Game.tsx
+++ frontend/src/components/game/Game.tsx
@@ -1,11 +1,27 @@
+const MAX_SCORE = 7
 
+const ScoreAnchor = ({isActive, direction}: {isActive: boolean, direction: string}) => (
+	<div className={`w-5 h-5 lg:w-7 lg:h-7 xl:w-10 xl:h-10 rounded-full bg-primary
+	${isActive ? 'bg-opacity-50' : ''}`
+	}></div>
+)
+
+const pointsCalculator = (direction: string) => {
+	const anchors = []
+	for (let i = 0; i < MAX_SCORE; i++) {
+		anchors.push(<ScoreAnchor key={i} isActive={i < 3 ? true : false} direction={direction} />)
+	}
+	return anchors
+}
 
 const PointsScreen = ({direction}: {direction: string}) => {
 	return (
-		<div className={`w-1/3 h-full bg-buttonColor rounded-[20%] bg-opacity-[90%] absolute 
-			${direction === 'right' ? 'right-[92%]' : 'left-[92%]'}
+		<div className={`w-1/12 h-full bg-buttonColor bg-opacity-[90%] absolute 
+			${direction === 'right' ? 'right-[92%] rounded-r-[35%]' : 'left-[92%] rounded-l-[35%]'}
+			flex flex-col gap-2 lg:gap-4 justify-center items-center
+			
 		`}>
-
+			{pointsCalculator(direction)}
 		</div>
 	)
 }
@@ -13,8 +29,8 @@ const PointsScreen = ({direction}: {direction: string}) => {
 const Paddle = ({side, position}: {side: string, position: {zbi: string}}) => {
 	return (
 		<div id={`paddle-${side}`}
-			className={`bg-background h-[20%] w-2 rounded-full absolute lg:w-3
-				${side === 'right' ? 'right-1' : 'left-1'}`}
+			className={`bg-background h-[20%] w-2 rounded-full absolute
+				lg:w-3 ${side === 'right' ? 'right-1' : 'left-1'}`}
 		></div>
 	)
 }
@@ -30,7 +46,12 @@ const Ball = () => {
 const PlayGround = () => {
 	const position = {zbi: 'test'}
 	return (
-		<div className={`absolute p-1 w-3/4 h-3/4 top-[12.5%] right-[12.5%] bg-secondary bg-opacity-[85%] rounded-lg`}>
+		<div id="play-ground" className={`absolute w-3/4 h-3/4 top-[12.5%] right-[12.5%]
+			bg-secondary bg-opacity-[85%] rounded-lg`}
+			>
+			<div className={`absolute w-1 h-full bg-sucessColor left-[49%]`}></div>
+			<div className={`absolute w-[25%] h-[25%] rounded-full  left-[36.9%] top-[37.5%]
+				border-[.25rem] border-solid border-sucessColor`}></div>
 			<Ball />
 			<Paddle side="right" position={position} />
 			<Paddle side="left" position={position} />
@@ -42,8 +63,8 @@ const GameField = () => {
 
 	return (
 		<div className="w-full h-full bg-sucessColor overflow-hidden relative">
-			<PointsScreen direction="right" />
 			<PlayGround />
+			<PointsScreen direction="right" />
 			<PointsScreen direction="left" />
 		</div>
 	)
--- frontend/src/components/game/index.tsx
+++ frontend/src/components/game/index.tsx
@@ -2,6 +2,7 @@ import Navbar from "../Navbar";
 import NotificationBar from "../notifbar/NotificationBar";
 import SideBar from "../sidebar/SideBar";
 import GameField from "./Game";
+import GameSetup from "./GameSetup";
 
 const GameFooter = () => (
 	<div className={`border-solid border-textColor border-4 shadow-gameShadow w-full h-10
@@ -16,7 +17,7 @@ const GameFooter = () => (
 const GameFrame = () => {
 
 	return (
-		<div className={`border-solid border-textColor border-4 shadow-gameShadow w-full
+		<div className={`border-solid border-textColor border-4 w-full
 		h-[576px] sm:h-[324px] md:h-[389px] lg:h-[518px] xl:h-[648px] 2xl:h-[757px]
 		rounded-t `}>
 			<GameField />
@@ -29,7 +30,8 @@ const GameUI = () => (
 		h-screen`}>
 		<div className={`flex flex-col items-center p-1 sm:p-5 mt-6 inset-0
 			w-[85%] sm:w-[576px] md:w-[691px] lg:w-[921px] xl:w-[1152px] 2xl:w-[1346px]`}>
-			<GameFrame />
+			<GameSetup />
+			{/* <GameFrame /> */}
 			<GameFooter />
 		</div>
 	</div>
@@ -37,12 +39,12 @@ const GameUI = () => (
 
 const GameMain = () => {
 	return (
-		<>
+		<div className="overflow-hidden">
 			<Navbar />
+			<GameUI />
 			<NotificationBar />
 			<SideBar />
-			<GameUI />
-		</>
+		</div>
 	)
 }
 
