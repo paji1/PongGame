0480b4a3921b87ba83d84107cf54e85d60b4069d
paji1 <tahamaroc1@gmail.com>
2023-12-14 06:08:53 +0100

    ~del: strict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -29,10 +29,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <currentUser.Provider value={user} >
 		<RouterProvider router={router} />
 	</currentUser.Provider>
-  </React.StrictMode>
 );
 
