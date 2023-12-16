fea3a8cd64d3a3ed9bd7f796579ef06aa8a2544b
paji1 <tahamaroc1@gmail.com>
2023-12-16 22:52:14 +0100

    details d zap

--- frontend/src/components/Common.tsx
+++ frontend/src/components/Common.tsx
@@ -1,6 +1,6 @@
 export const HoverDiv = ({toggleChatBar}: {toggleChatBar: any}) => (
 	<div
-	className="fixed inset-0 bg-textColor bg-opacity-50 z-50 transition-all duration-300"
+	className="fixed inset-0 bg-textColor bg-opacity-50 z-40 transition-all duration-300"
 	onClick={() => toggleChatBar()}
 	aria-hidden="true"
 	></div>
