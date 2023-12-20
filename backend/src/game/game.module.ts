576b3ba01987f9ef3fc5763bc8a2a1fbe4b62400
paji1 <tahamaroc1@gmail.com>
2023-12-20 04:32:04 +0100

    <add>: GameMatchingService, GameGateway

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { GameMatchingService } from './game-matching/game-matching.service';
+import { GameGateway } from './game.gateway';
 
 
 @Module({
 	imports: [PrismaModule],
 	controllers: [GameController],
-	providers: [GameService],
+	providers: [GameService, GameMatchingService, GameGateway],
 	
 })
 export class GameModule {}
