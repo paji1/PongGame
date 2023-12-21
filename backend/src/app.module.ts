152dbc0b307ef1e71b1f3eb4afe0c8fc423bc987
paji1 <tahamaroc1@gmail.com>
2023-12-22 00:08:25 +0100

    merge socket handshake from chat

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,22 +2,24 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ChatModule } from "./chat/chat.module";
 import { AuthModule } from "./auth/auth.module";
-import { UsersService } from "./users/users.service";
 import { UsersModule } from "./users/users.module";
 import { AtGuard } from "./common/guards";
 import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
+import { InviteModule } from "./invite/invite.module";
 import { GameModule } from "./game/game.module";
-import { GameGateway } from "./game/game.gateway";
+import { AppGateway } from './app.gateway';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ChatModule, AuthModule, UsersModule, GameModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ChatModule, AuthModule, UsersModule, InviteModule],
 
 	providers: [
+		
 		{
 			provide: APP_GUARD,
 			useClass: AtGuard,
-		}
+		},
+		AppGateway,
 	],
 })
 export class AppModule {}
