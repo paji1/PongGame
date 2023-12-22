912c5f7aa6a50090ee4ca0c97fc03f60132a2470
paji1 <tahamaroc1@gmail.com>
2023-12-22 01:06:02 +0100

    DTO's

--- backend/src/game/dto/matching-dto.dto.ts
+++ backend/src/game/dto/matching-dto.dto.ts
@@ -2,13 +2,13 @@ import { IsEnum, IsString, MinLength } from "class-validator"
 import { EDifficulty, EMatchingType } from "src/types.ts/game-matching.interface"
 
 export class  MatchingGameDto {
-	@IsEnum(EDifficulty)
+	@IsEnum(EDifficulty, {message: "Invalid game mode"})
 	difficulty: EDifficulty
 
-	@IsEnum(EMatchingType)
+	@IsEnum(EMatchingType, {message: "Invalid matching system"})
 	matchingType: EMatchingType
 
 	@IsString()
-	@MinLength(3)
+	@MinLength(3, {message: "Invalid nickname"})
 	invite: string
 }
